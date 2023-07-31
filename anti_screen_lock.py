2182896a3d950b7fec6cc3f6e6a1a87583464fb7
Alexey Zaytsev <alex.rv9rh@gmail.com>
2023-07-31 06:30:52 +0500

    Add config file, refactoring..

--- anti_screen_lock.py
+++ anti_screen_lock.py
@@ -1,12 +1,53 @@
 
+import os
 import win32api
 import time
 from pynput.keyboard import Key, Controller
+import configparser
 
 
-MAX_COUNT = 55
-count = 0
-prev_last_input_info = 0
+CONFIG_FILE = "anti_screen_lock.ini"
+DEFAULT_CONFIG = {
+    # 30 x 6 = 180 seconds = 3 min
+    "main_cycle_delay": 30,
+    "max_count": 6,
+    "debug_print": True,
+}
+config = {}
+
+
+def load_config():
+    global config
+
+    def load_key(parser, key, type="str"):
+        try:
+            if type == "str":
+                value = parser["DEFAULT"][key]
+            elif type == "int":
+                value = int(parser["DEFAULT"][key])
+            elif type == "float":
+                value = float(parser["DEFAULT"][key])
+            elif type == "bool":
+                value = bool(int(parser["DEFAULT"][key]))
+        except KeyError:
+            print(f"No key '{key}' in config file! Loaded from DEFAULT_CONFIG")
+            value = DEFAULT_CONFIG[key]    
+
+        return value
+
+
+    if not os.path.exists(CONFIG_FILE):
+        print("Config file not found! Loaded DEFAULT_CONFIG")
+        config = DEFAULT_CONFIG
+        return
+
+    parser = configparser.ConfigParser()
+    parser.read(CONFIG_FILE, encoding="utf-8")
+
+    config["main_cycle_delay"] = load_key(parser, "main_cycle_delay", "int")
+    config["max_count"] = load_key(parser, "max_count", "int")
+    config["debug_print"] = load_key(parser, "debug_print", "bool")
+
 
 keyboard = Controller()
 
@@ -18,16 +59,28 @@ def blink_caps_lock():
     keyboard.release(Key.caps_lock)
 
 
-while True:
-    last_input_info = win32api.GetLastInputInfo()
+def main():
+    load_config()
+    print(config)
+    
+    count = 0
+    prev_last_input_info = 0
 
-    if last_input_info == prev_last_input_info:
-        count += 1
-        if count == MAX_COUNT:
-            # print("lock screen time!")
-            blink_caps_lock()
-    else:
-        count = 0
+    while True:
+        last_input_info = win32api.GetLastInputInfo()
 
-    prev_last_input_info = last_input_info
-    time.sleep(1)
+        if last_input_info == prev_last_input_info:
+            count += 1
+            if count == config["max_count"]:
+                if config["debug_print"]:
+                    print("lock screen time!")
+                blink_caps_lock()
+        else:
+            count = 0
+
+        prev_last_input_info = last_input_info
+        time.sleep(config["main_cycle_delay"])
+
+
+if __name__ == "__main__":
+    main()
